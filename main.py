40fa5b4ec10bc6b3cd479e2a0a44ebdc2584189d
Nguyenvanhieu229 <hieunguyenduyenhai22092002@gmail.com>
2025-06-16 23:53:36 +0700

    init

---
+++ main.py
@@ -0,0 +1,19 @@
+import getpass
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if "GOOGLE_API_KEY" not in os.environ:
+    os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your Google AI API key: ")
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
+
